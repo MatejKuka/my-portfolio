f3de371f81c5b82076b8deff630952a678b92132
Matej <mate1778@easv365.dk>
2024-03-31 23:39:55 +0200

    fsdfsd

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -8,7 +8,7 @@ import GithubImage from "../assets/icons8-github-60.png";
 
 const MAIN_TEXT = "I’m Matej Kuka";
 const SECONDARY_TEXT = "Fullstack developer using his passion for innovation, start-ups and entrepreneurship";
-const TERCIARY_TEXT = "Currently based in Copenhagen, Denmark";
+const TERCIARY_TEXT = "Currently based in Bratislava, Slovakia";
 
 function LandingPage() {
     return (
